20937e030c6a17361a4df95b314c8f2d240d05a1
codenesium <codenesium@gmail.com>
2020-06-19 20:13:29 +0000

    added strongly typed settings article

---
+++ StronglyTypedConfiguration/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace StronglyTypedConfiguration
+{
+    public class ApiSettings
+    {
+        public string AdminEmail { get; set; }
+        public JwtSettings JwtSettings { get; set; }
+    }
+
+    public class JwtSettings
+    {
+        public string SigningKey { get; set; }
+    }
+}
---
+++ StronglyTypedConfiguration/Controllers/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace StronglyTypedConfiguration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SettingsController : ControllerBase
+    {
+
+        private readonly ApiSettings apiSettings;
+        private readonly ILogger<SettingsController> logger;
+
+        public SettingsController(IOptions<ApiSettings> apiSettings, ILogger<SettingsController> logger)
+        {
+            this.apiSettings = apiSettings.Value;
+            this.logger = logger;
+        }
+       
+        [HttpGet]
+        public async Task <ActionResult<string>> GetAdminEmail()
+        {
+            return this.apiSettings.AdminEmail;
+        }
+    }
+}
